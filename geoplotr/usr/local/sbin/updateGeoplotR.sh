26508250c9143114900c4c7a59c0c1ef4057bf03
Tim Band <t.band@nnnn.com>
2022-07-20 12:16:26 +0100

    updateGeoplotR.sh script updates shinylight

--- geoplotr/usr/local/sbin/updateGeoplotR.sh
+++ geoplotr/usr/local/sbin/updateGeoplotR.sh
@@ -1,3 +1,3 @@
-sudo -Hu wwwrunner Rscript -e "remotes::install_github(repo=c('pvermees/GeoplotR','pvermees/GeoplotRgui'),force=TRUE,lib='~/R')"
+sudo -Hu wwwrunner Rscript -e "remotes::install_github(repo=c('tim-band/shinylight','pvermees/GeoplotR','pvermees/GeoplotRgui'),force=TRUE,lib='~/R')"
 # We use sudo here so that the user only gets asked for a password once
 sudo /usr/local/sbin/geoplotrctl restart
