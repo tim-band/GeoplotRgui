702e0693c09cbc541e77c489e9d4c99b9e9f2ac4
Tim Band <t.band@nnnn.com>
2022-10-04 12:35:35 +0100

    Added a hierarchy to the function menu

--- R/geoplotr.R
+++ R/geoplotr.R
@@ -24,6 +24,20 @@ getCathColumn <- function(column) {
   cath[[column]][cascades]
 }
 
+functiongroups <- list(
+  trees = list(
+    "cart_all",
+    "cart_HFS",
+    "cart_ratios"
+  ),
+  others = list(
+    "AFM", "ATM", "AnAbOr", "CrY", "LaYb", "NbLaYb", "NbZrY", "QAPF",
+    "SrY", "TAS", "ThCo", "ThNbLaYb", "ThTaHf", "TiSiSr", "LuEuSr",
+    "TiVSc", "TiZrYSr", "Pearce1976", "NbNaSr", "TiSmV", "TiV",
+    "TiZrY", "YbTa", "YbTaRb", "YNb", "YNbRb", "ZrTi"
+  )
+)
+
 functions <- list(
   AFM=list(
     params=list(
@@ -771,8 +785,11 @@ GeoplotR <- function(host='0.0.0.0', port=NULL, daemonize=FALSE) {
       YNbRb = GeoplotR::YNbRb,
       ZrTi = ZrTi,
       getSchema = function() {
-        list(functions=functions, params=params, types=types,
-          data=examples, optiongroups=optiongroups)
+        list(
+          functiongroups = functiongroups,
+          functions = functions, params = params, types = types,
+          data = examples, optiongroups = optiongroups
+        )
       }
     )
   )
